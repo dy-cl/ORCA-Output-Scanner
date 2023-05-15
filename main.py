216d5028ccf3938e85335da7fabbb74693039b28
dylan <dylanrcounsell@gmail.com>
2023-05-15 11:13:49 +0100

    menu

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('test')
+def menu():
+    print('Select File Type: ')
+    print('1. ORCA')
+    print('2. N/A')
+    print('3. N/A')
+    print('4. N/A')
+  
+    choice = input('Enter Choice (1-4): ')
+    return choice
+
+user_choice = menu()
+print('Your Choice is ' + user_choice)
