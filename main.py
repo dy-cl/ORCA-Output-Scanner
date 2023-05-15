b5adcb55160ad9d69cc6022a1f476b3c375c35c0
dylan <dylanrcounsell@gmail.com>
2023-05-15 12:27:12 +0100

    functions

--- main.py
+++ main.py
@@ -1,44 +1,66 @@
-#_______ENTER OWN FILE PATH HERE_____________
-file_path = '/home/dylan/Python Projects/ORCA File Scanner/water.txt'
+from matplotlib import pyplot as plt
+import numpy as np
 
 #Select file type
-def menu():
-    print('Select File Type: ')
+def select_file_type():
+    print('Select File Type:')
     print('1. ORCA')
     print('2. N/A')
     print('3. N/A')
     print('4. N/A')
-  
+
     choice = input('Enter Choice (1 - 4): ')
     return choice
 
-#Run menu and check choice is returned correctly
-software_choice = menu()
-print('Your Choice is ' + software_choice + '\n')
+#Select ORCA task
+def select_orca_task():
+    print('Select Task:')
+    print('1. Final Single Point Energy')
+    print('2. Geometry Optimization Steps')
 
-#Select task in ORCA
-def orca_menu():
-    print('Select Task: ')
-    print('1. Final Single Point Energy: ')
-
-    choice = input ('Enter Choice (1 - 1): ')
+    choice = input('Enter Choice (1 - 2): ')
     return choice
 
+#Get the final single point energy from ORCA file
+def get_final_single_point_energy(file_path):
+    last_SPE = None
 
-#ORCA Files
-if software_choice == '1':
+    with open(file_path, 'r') as file:
+        for line in file:
+            if 'FINAL SINGLE POINT ENERGY' in line:
+                last_SPE = line
 
-    task_choice = orca_menu()
-    
-    #Final Single Point Energues
-    if task_choice == '1':
-        found = False
-        with open(file_path, 'r') as file:
-            for line in file:
-                if 'FINAL SINGLE POINT ENERGY' in line:
-                    print(line)
-                    found = True
-                    break
-        if not found:
-            print('Final Single Point Energy: Not Found')
+    if last_SPE:
+        print('Final Single Point Energy: ' + last_SPE)
+    else:
+        print('Final Single Point Energy: Not Found')
 
+#Get the geometry optimization steps from ORCA file and plot the energies
+def plot_geometry_optimization_steps(file_path):
+    energies = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            if 'FINAL SINGLE POINT ENERGY' in line:
+                energies.append(float(line.split()[-1].strip()))
+
+    plt.plot(range(len(energies)), energies)
+    plt.show()
+
+#Main function
+def main():
+    file_path = '/home/dylan/Python Projects/ORCA File Scanner/benzyne.txt'
+    software_choice = select_file_type()
+    print('Your Choice is ' + software_choice + '\n')
+
+    if software_choice == '1':
+        task_choice = select_orca_task()
+
+        if task_choice == '1':
+            get_final_single_point_energy(file_path)
+        elif task_choice == '2':
+            plot_geometry_optimization_steps(file_path)
+
+# Run the main function
+if __name__ == '__main__':
+    main()
